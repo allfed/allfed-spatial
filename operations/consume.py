dba6f342387edb6418a90c4dc325e5beb776d559
Tim Fist <timf@dasintel.io>
2019-06-13 00:24:09 +1000

    Network consumption operation function with test

--- operations/consume.py
+++ operations/consume.py
@@ -1,16 +1,48 @@
-def consume(source, sink, source_attr="remaining", sink_attr="remaining"):
+
+
+def consume(
+    source, sink, source_attr="remaining", sink_attr="remaining", efficiency=1
+):
+    """
+    Each source has an 'effective supply', which is the maximum demand
+    that it can meet based on efficiency. When consumption occurs, supply
+    will be decremented by the efficiency-adjusted amount, whereas demand
+    will be decremented by the non-adjusted amount.
+
+    Arguments:
+        source {[type]} -- [description]
+        sink {[type]} -- [description]
+
+    Keyword Arguments:
+        source_attr {str} -- [description] (default: {"remaining"})
+        sink_attr {str} -- [description] (default: {"remaining"})
+        efficiency {int} -- [description] (default: {1})
+
+    Returns:
+        [type] -- [description]
+    """
+
     supply, demand = source[source_attr], sink[sink_attr]
+    effective_supply = efficiency * supply
+
     if (supply == 0 or demand == 0):
-        return source, sink
-    if supply > demand:
-        source[source_attr] = supply - demand
+        consumed = 0
+        return source, sink, consumed
+
+    if effective_supply > demand:
+        consumed = demand
+        source[source_attr] = supply - (consumed / efficiency)
         sink[source_attr] = 0
-        return source, sink
-    if supply == demand:
+        return source, sink, consumed
+
+    if effective_supply == demand:
+        consumed = effective_supply
         source[source_attr] = 0
         sink[source_attr] = 0
-        return source, sink
-    if supply < demand:
+        return source, sink, consumed
+
+    if effective_supply < demand:
+        consumed = effective_supply
         source[source_attr] = 0
-        sink[source_attr] = demand - supply
-        return source, sink
+        sink[source_attr] = demand - effective_supply
+        return source, sink, consumed
---
+++ tests/test_consume.py
@@ -0,0 +1,129 @@
+import unittest
+from operations.consume import consume
+
+
+class TestConsume(unittest.TestCase):
+
+    def test_zero_source_consumption(self):
+        source = {'remaining': 0}
+        sink = {'remaining': 100}
+        self.assertEqual(
+            consume(source, sink),
+            (
+                {'remaining': 0},
+                {'remaining': 100},
+                0
+            )
+        )
+
+    def test_zero_source_consumption_with_efficiency(self):
+        source = {'remaining': 0}
+        sink = {'remaining': 100}
+        self.assertEqual(
+            consume(source, sink, efficiency=0.6),
+            (
+                {'remaining': 0},
+                {'remaining': 100},
+                0
+            )
+        )
+
+    def test_zero_sink_consumption(self):
+        source = {'remaining': 100}
+        sink = {'remaining': 0}
+        self.assertEqual(
+            consume(source, sink),
+            (
+                {'remaining': 100},
+                {'remaining': 0},
+                0
+            )
+        )
+
+    def test_zero_sink_consumption_with_efficiency(self):
+        source = {'remaining': 100}
+        sink = {'remaining': 0}
+        self.assertEqual(
+            consume(source, sink, efficiency=0.6),
+            (
+                {'remaining': 100},
+                {'remaining': 0},
+                0
+            )
+        )
+
+    def test_full_consumption(self):
+        source = {'remaining': 100}
+        sink = {'remaining': 100}
+        self.assertEqual(
+            consume(source, sink),
+            (
+                {'remaining': 0},
+                {'remaining': 0},
+                100
+            )
+        )
+
+    def test_full_consumption_with_efficiency(self):
+        source = {'remaining': 100}
+        sink = {'remaining': 100}
+        self.assertEqual(
+            consume(source, sink, efficiency=0.6),
+            (
+                {'remaining': 0},
+                {'remaining': 40},
+                60
+            )
+        )
+
+    def test_partial_source_consumption(self):
+        source = {'remaining': 100}
+        sink = {'remaining': 50}
+        self.assertEqual(
+            consume(source, sink),
+            (
+                {'remaining': 50},
+                {'remaining': 0},
+                50
+            )
+        )
+
+    def test_partial_source_consumption_with_efficiency(self):
+        source = {'remaining': 100}
+        sink = {'remaining': 50}
+        self.assertEqual(
+            consume(source, sink, efficiency=0.6),
+            (
+                {'remaining': 16.666666666666657},
+                {'remaining': 0},
+                50
+            )
+        )
+
+    def test_partial_sink_consumption(self):
+        source = {'remaining': 50}
+        sink = {'remaining': 100}
+        self.assertEqual(
+            consume(source, sink),
+            (
+                {'remaining': 0},
+                {'remaining': 50},
+                50
+            )
+        )
+
+    def test_partial_sink_consumption_with_efficiency(self):
+        source = {'remaining': 50}
+        sink = {'remaining': 100}
+        self.assertEqual(
+            consume(source, sink, efficiency=0.6),
+            (
+                {'remaining': 0},
+                {'remaining': 70},
+                30
+            )
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
