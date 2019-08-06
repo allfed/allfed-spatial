d8d07aeed5c0c36cef2805d3e20d7ddd89e84ffc
Tim Fist <timf@dasintel.io>
2019-08-06 10:35:17 +1000

    add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,17 @@ setup(
     name='allfed-spatial',
     version='0.1',
     packages=find_packages(),
+    install_requires=[
+        'fiona',
+        'shapely',
+        'pyproj',
+        'rasterio',
+        'rtree',
+        'ortools',
+        'affine',
+        'numpy',
+        'fuzzywuzzy'
+    ],
     url='https://github.com/allfed/allfed-spatial',
     license='MIT',
     author='ALLFED',
