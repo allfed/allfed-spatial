09215ce48ad8081999ec46c26fe0ca5a31f4b1f1
Athight <j.lallu25@gmail.com>
2020-03-03 21:17:29 +1300

    note windows requirements

--- allfed_spatial/features/io.py
+++ allfed_spatial/features/io.py
@@ -1,4 +1,6 @@
 import fiona
+# Windows requires fiona 1.8.9+ (1.8.13 confirmed) and GDAL 3.0.2+ (3.0.4 confirmed)
+# This is because Fiona 1.8.9 adds support for GDAL 3 and rasterio requires GDAL 3
 from shapely.geometry import mapping, LineString, Polygon, Point, shape, MultiPolygon
 from fiona.crs import from_epsg
 
