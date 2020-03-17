ff8fa33632e5a5905609afac01ef124eeedf9820
Athight <j.lallu25@gmail.com>
2020-03-17 20:23:02 +1300

    test write_features and load_features

--- tests/test_features_io.py
+++ tests/test_features_io.py
@@ -2,6 +2,7 @@ import os
 import tempfile
 import unittest
 import allfed_spatial.features.io as featureIO
+import fiona
 from shapely.geometry import LineString, LinearRing, MultiPolygon, Point, Polygon
 from allfed_spatial.features.feature import Feature
 from tests.test_geometry_line import LineBaseTest
@@ -149,7 +150,7 @@ complete_test_data_2 = {
 	"uniqueKey": 2
 }
 
-class Test_write_features(unittest.TestCase):
+class Test_write_features(LineBaseTest):
 	def test_line_string(self):
 		with tempfile.TemporaryDirectory("-allfed-spatial-test") as tempdir:
 			filename = os.path.join(tempdir, "testfile.file")
@@ -161,12 +162,91 @@ class Test_write_features(unittest.TestCase):
 				LineString([(2, 2), (3, 3)]),
 				complete_test_data_2)
 
-			featureIO.write_features([feature1, feature2], filename)
-			self.fail("test to be written")
+			featuresToDisk = [feature1, feature2]
+			featureIO.write_features(featuresToDisk, filename)
+			featuresFromDisk = featureIO.load_features(filename)
+			self.FeaturesEqual(featuresFromDisk, featuresToDisk)
 
-class Test_load_features(unittest.TestCase):
-	def test_line_string(self):
-		self.fail("test to be written")
+	def test_point(self):
+		with tempfile.TemporaryDirectory("-allfed-spatial-test") as tempdir:
+			filename = os.path.join(tempdir, "testfile.file")
+
+			feature1 = Feature(
+				Point(0, 0),
+				complete_test_data_1)
+			feature2 = Feature(
+				Point(1, 1),
+				complete_test_data_2)
+
+			featuresToDisk = [feature1, feature2]
+			featureIO.write_features(featuresToDisk, filename)
+			featuresFromDisk = featureIO.load_features(filename)
+			self.FeaturesEqual(featuresFromDisk, featuresToDisk)
+
+	def test_polygon(self):
+		with tempfile.TemporaryDirectory("-allfed-spatial-test") as tempdir:
+			filename = os.path.join(tempdir, "testfile.file")
+
+			feature1 = Feature(
+				Polygon([(0, 0), (1, 1), (0, 1)]),
+				complete_test_data_1)
+			feature2 = Feature(
+				Polygon([(0, 0), (1, 1), (1, 0)]),
+				complete_test_data_2)
+
+			featuresToDisk = [feature1, feature2]
+			featureIO.write_features(featuresToDisk, filename)
+			featuresFromDisk = featureIO.load_features(filename)
+			self.FeaturesEqual(featuresFromDisk, featuresToDisk)
+
+	def test_multi_polygon(self):
+		with tempfile.TemporaryDirectory("-allfed-spatial-test") as tempdir:
+			filename = os.path.join(tempdir, "testfile.file")
+
+			feature1 = Feature(
+				MultiPolygon([
+					Polygon([(0, 0), (1, 1), (0, 1)]),
+					Polygon([(0, 0), (1, 1), (1, 0)])]),
+				complete_test_data_1)
+			feature2 = Feature(
+				MultiPolygon([
+					Polygon([(1, 1), (2, 2), (1, 2)]),
+					Polygon([(1, 1), (2, 2), (2, 1)])]),
+				complete_test_data_2)
+
+			featuresToDisk = [feature1, feature2]
+			featureIO.write_features(featuresToDisk, filename)
+			featuresFromDisk = featureIO.load_features(filename)
+			self.FeaturesEqual(featuresFromDisk, featuresToDisk)
+
+	def test_other_geom(self):
+		"""Other geometry is silently lost, no exceptions stop the process"""
+		with tempfile.TemporaryDirectory("-allfed-spatial-test") as tempdir:
+			filename = os.path.join(tempdir, "testfile.file")
+
+			feature1 = Feature(
+				LinearRing([(0, 0), (1, 1), (0, 1)]),
+				complete_test_data_1)
+
+			featuresToDisk = [feature1]
+			featureIO.write_features(featuresToDisk, filename)
+			featuresFromDisk = featureIO.load_features(filename)
+			self.assertEqual(featuresFromDisk, [])
+
+	def test_mixed_geom(self):
+		with tempfile.TemporaryDirectory("-allfed-spatial-test") as tempdir:
+			filename = os.path.join(tempdir, "testfile.file")
+
+			feature1 = Feature(
+				LineString([(0, 0), (1, 1)]),
+				complete_test_data_1)
+			feature2 = Feature(
+				Point(20, 20),
+				complete_test_data_2)
+
+			featuresToDisk = [feature1, feature2]
+			with self.assertRaises(fiona.errors.GeometryTypeValidationError):
+				featureIO.write_features(featuresToDisk, filename)
 
 class Test_write_shape(unittest.TestCase):
 	def test_line_string(self):
