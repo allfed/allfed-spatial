44d09ebc74bccc6301c707ba8a7401fcfc4646cb
Athight <j.lallu25@gmail.com>
2020-03-24 21:07:59 +1300

    Update test_features_io.py

--- tests/test_features_io.py
+++ tests/test_features_io.py
@@ -3,6 +3,7 @@ import tempfile
 import unittest
 import allfed_spatial.features.io as featureIO
 import fiona
+import math
 from shapely.geometry import LineString, LinearRing, MultiPolygon, Point, Polygon
 from allfed_spatial.features.feature import Feature
 from tests.test_geometry_line import LineBaseTest
@@ -150,6 +151,42 @@ complete_test_data_2 = {
 	"uniqueKey": 2
 }
 
+diff_schema_test_data = {
+	"int": 123,
+	"float": 123.4,
+	"string": "this is a test string",
+	"uniqueKey": 3,
+	"new Key": "breaks the schema"
+}
+
+diff_schema_convertable_string_test_data = {
+	"int": 123,
+	"float": 123.4,
+	"string": "the unique key is a string, technically the schema is different",
+	"uniqueKey": "3"
+}
+
+diff_schema_convertable_float_test_data = {
+	"int": 123,
+	"float": 123.4,
+	"string": "the unique key is a float, technically the schema is different",
+	"uniqueKey": 3.5
+}
+
+diff_schema_unconvertable_string_test_data = {
+	"int": 123,
+	"float": 123.4,
+	"string": "the unique key is not convertable to an int",
+	"uniqueKey": "NaN"
+}
+
+diff_schema_unconvertable_float_test_data = {
+	"int": 123,
+	"float": 123.4,
+	"string": "the unique key is not convertable to an int",
+	"uniqueKey": math.nan
+}
+
 class Test_write_features(LineBaseTest):
 	def test_line_string(self):
 		with tempfile.TemporaryDirectory("-allfed-spatial-test") as tempdir:
@@ -247,6 +284,119 @@ class Test_write_features(LineBaseTest):
 			featuresToDisk = [feature1, feature2]
 			with self.assertRaises(fiona.errors.GeometryTypeValidationError):
 				featureIO.write_features(featuresToDisk, filename)
+
+	def test_different_schema(self):
+		with tempfile.TemporaryDirectory("-allfed-spatial-test") as tempdir:
+			filename = os.path.join(tempdir, "testfile.file")
+
+			feature1 = Feature(
+				LineString([(0, 0), (1, 1)]),
+				complete_test_data_1)
+			feature2 = Feature(
+				LineString([(2, 2), (3, 3)]),
+				diff_schema_test_data)
+
+			featuresToDisk = [feature1, feature2]
+			with self.assertRaises(ValueError):
+				featureIO.write_features(featuresToDisk, filename)
+
+	def test_different_schema_convertable_string(self):
+		"""Note that the string is converted to an int"""
+		with tempfile.TemporaryDirectory("-allfed-spatial-test") as tempdir:
+			filename = os.path.join(tempdir, "testfile.file")
+
+			feature1 = Feature(
+				LineString([(0, 0), (1, 1)]),
+				complete_test_data_1)
+			feature2 = Feature(
+				LineString([(2, 2), (3, 3)]),
+				diff_schema_convertable_string_test_data)
+
+			featuresToDisk = [feature1, feature2]
+			featureIO.write_features(featuresToDisk, filename)
+
+			featuresFromDisk = featureIO.load_features(filename)
+
+			modifiedFeaturesFromDisk = featureIO.load_features(filename)
+			modifiedFeaturesFromDisk[1].data["uniqueKey"] = "3"
+
+			self.assertNotEqual(featuresFromDisk[1].data["uniqueKey"], "3")
+			self.assertEqual(featuresFromDisk[1].data["uniqueKey"], 3)
+			self.FeaturesEqual(modifiedFeaturesFromDisk, featuresToDisk)
+
+	def test_different_schema_convertable_float(self):
+		"""Note that the float is converted to an int"""
+		with tempfile.TemporaryDirectory("-allfed-spatial-test") as tempdir:
+			filename = os.path.join(tempdir, "testfile.file")
+
+			feature1 = Feature(
+				LineString([(0, 0), (1, 1)]),
+				complete_test_data_1)
+			feature2 = Feature(
+				LineString([(2, 2), (3, 3)]),
+				diff_schema_convertable_float_test_data)
+
+			featuresToDisk = [feature1, feature2]
+			featureIO.write_features(featuresToDisk, filename)
+
+			featuresFromDisk = featureIO.load_features(filename)
+
+			modifiedFeaturesFromDisk = featureIO.load_features(filename)
+			modifiedFeaturesFromDisk[1].data["uniqueKey"] = 3.5
+
+			self.assertNotEqual(featuresFromDisk[1].data["uniqueKey"], 3.5)
+			self.assertEqual(featuresFromDisk[1].data["uniqueKey"], 3)
+			self.FeaturesEqual(modifiedFeaturesFromDisk, featuresToDisk)
+
+	def test_different_schema_unconvertable_string(self):
+		"""Note that the string value is dropped and a default of 0 is used"""
+		with tempfile.TemporaryDirectory("-allfed-spatial-test") as tempdir:
+			filename = os.path.join(tempdir, "testfile.file")
+
+			feature1 = Feature(
+				LineString([(0, 0), (1, 1)]),
+				complete_test_data_1)
+			feature2 = Feature(
+				LineString([(2, 2), (3, 3)]),
+				diff_schema_unconvertable_string_test_data)
+
+			featuresToDisk = [feature1, feature2]
+			featureIO.write_features(featuresToDisk, filename)
+
+			featuresFromDisk = featureIO.load_features(filename)
+
+			modifiedFeaturesFromDisk = featureIO.load_features(filename)
+			modifiedFeaturesFromDisk[1].data["uniqueKey"] = "NaN"
+
+			self.assertNotEqual(featuresFromDisk[1].data["uniqueKey"], "NaN")
+			self.assertEqual(featuresFromDisk[1].data["uniqueKey"], 0)
+			self.FeaturesEqual(modifiedFeaturesFromDisk, featuresToDisk)
+
+	def test_different_schema_unconvertable_float(self):
+		"""Note that the NaN value is dropped,
+		it seems like int_64(1<<64) is used
+		i.e. -(2**63), the minimum 64 bit int value
+		
+		odd given that we map to int:16,
+		TODO: maybe we should be mapping to int:64 instead,
+		we'll find out in the write_shape tests."""
+		with tempfile.TemporaryDirectory("-allfed-spatial-test") as tempdir:
+			filename = os.path.join(tempdir, "testfile.file")
+
+			feature1 = Feature(
+				LineString([(0, 0), (1, 1)]),
+				complete_test_data_1)
+			feature2 = Feature(
+				LineString([(2, 2), (3, 3)]),
+				diff_schema_unconvertable_float_test_data)
+
+			featuresToDisk = [feature1, feature2]
+			featureIO.write_features(featuresToDisk, filename)
+
+			featuresFromDisk = featureIO.load_features(filename)
+
+			self.assertEqual(featuresFromDisk[1].data["uniqueKey"], -(2**63))
+			self.assertTrue(math.isnan(featuresToDisk[1].data["uniqueKey"]))
 
 class Test_write_shape(unittest.TestCase):
 	def test_line_string(self):
