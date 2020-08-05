62059b3e32389e71ce7522e1a08d15d1cbcfadd1
Athight <j.lallu25@gmail.com>
2020-08-05 17:50:04 +1200

    test preprocess_graph

--- tests/test_flow.py
+++ tests/test_flow.py
@@ -8,6 +8,7 @@ import networkx as nx
 from shapely.geometry import LineString, LinearRing, MultiPolygon, Point, Polygon
 from allfed_spatial.features.feature import Feature
 from tests.test_geometry_line import LineBaseTest
+from allfed_spatial.operations.flow import *
 
 class Test_graph_handling(LineBaseTest):
 	def test_missing_file(self):
@@ -177,4 +178,149 @@ class Test_graph_handling(LineBaseTest):
 				self.assertTrue("some" in G[source][target].keys())
 
 class Test_preprocess_graph(LineBaseTest):
+	def test_none(self):
+		with self.assertRaises(AttributeError):
+			preprocess_graph(None)
+
+	def test_empty_file(self):
+		with tempfile.TemporaryDirectory("-allfed-spatial-test") as tempdir:
+			filename = os.path.join(tempdir, "testfile.shp")
+			schema = featureIO.get_feature_schema(Feature(Point(0, 0)))
+
+			featureIO.write_shape([], [], schema, filename, "ESRI Shapefile")
+
+			G = nx.read_shp(tempdir)
+			preprocess_graph(G)
+			
+			self.assertEqual(G.number_of_nodes(), 0, "Expected zero nodes")
+			self.assertEqual(G.number_of_edges(), 0, "Expected zero edges")
+
+
+	def test_single_node(self):
+		with tempfile.TemporaryDirectory("-allfed-spatial-test") as tempdir:
+			filename = os.path.join(tempdir, "testfile.shp")
+			features = [
+				Feature(Point(4, 2), {"some": "data"})
+			]
+
+			featureIO.write_features(features, filename, "ESRI Shapefile")
+
+			G = nx.read_shp(tempdir)
+			preprocess_graph(G)
+
+			self.assertEqual(G.number_of_nodes(), 1, "Expected one node")
+			self.assertEqual(G.number_of_edges(), 0, "Expected zero edges")
+			self.assertEqual(G.nodes().get((4, 2))["index"], 0)
+			self.assertEqual(G.nodes().get((4, 2))["diff"], 0)
+
+	def test_single_edge(self):
+		with tempfile.TemporaryDirectory("-allfed-spatial-test") as tempdir:
+			filename = os.path.join(tempdir, "testfile.shp")
+			features = [
+				Feature(LineString([(4, 2), (1, 2)]), {"some": "data"})
+			]
+
+			featureIO.write_features(features, filename, "ESRI Shapefile")
+
+			G = nx.read_shp(tempdir)
+			preprocess_graph(G)
+
+			self.assertEqual(G.number_of_nodes(), 2, "Expected two nodes")
+			self.assertEqual(G.number_of_edges(), 1, "Expected one edge")
+			indexs = []
+			for key in G.nodes():
+				node = G.nodes()[key]
+				self.assertEqual(node['diff'], 0)
+				indexs.append(node['index'])
+			self.assertEqual(len(indexs), 2)
+			indexs.sort()
+			self.assertEqual(indexs, list(range(2)))
+
+	def test_four_way_intersection(self):
+		with tempfile.TemporaryDirectory("-allfed-spatial-test") as tempdir:
+			filename = os.path.join(tempdir, "testfile.shp")
+			features = [
+				Feature(LineString([(0, 0), (1, 0)]), {"some": "data"}),
+				Feature(LineString([(0, 0), (0, 1)]), {"some": "data"}),
+				Feature(LineString([(0, 0), (-1, 0)]), {"some": "data"}),
+				Feature(LineString([(0, 0), (0, -1)]), {"some": "data"}),
+				Feature(LineString([(1, 0), (0, 0)]), {"some": "data"}),
+				Feature(LineString([(0, 1), (0, 0)]), {"some": "data"}),
+				Feature(LineString([(-1, 0), (0, 0)]), {"some": "other data"}),
+				Feature(LineString([(0, -1), (0, 0)]), {"some": "other data"}),
+			]
+
+			featureIO.write_features(features, filename, "ESRI Shapefile")
+
+			G = nx.read_shp(tempdir)
+			preprocess_graph(G)
+
+			self.assertEqual(G.number_of_nodes(), 5, "Expected five nodes")
+			self.assertEqual(G.number_of_edges(), 8, "Expected four edges")
+			indexs = []
+			for key in G.nodes():
+				node = G.nodes()[key]
+				self.assertEqual(node['diff'], 0)
+				indexs.append(node['index'])
+			self.assertEqual(len(indexs), 5)
+			indexs.sort()
+			self.assertEqual(indexs, list(range(5)))
+
+	def test_loop(self):
+		with tempfile.TemporaryDirectory("-allfed-spatial-test") as tempdir:
+			filename = os.path.join(tempdir, "testfile.shp")
+			features = [
+				Feature(LineString([(1, 0), (0, 1)]), {"some": "data"}),
+				Feature(LineString([(0, 1), (-1, 0)]), {"some": "data"}),
+				Feature(LineString([(-1, 0), (0, -1)]), {"some": "data"}),
+				Feature(LineString([(0, -1), (1, 0)]), {"some": "data"}),
+			]
+
+			featureIO.write_features(features, filename, "ESRI Shapefile")
+
+			G = nx.read_shp(tempdir)
+			preprocess_graph(G)
+
+			self.assertEqual(G.number_of_nodes(), 4, "Expected four nodes")
+			self.assertEqual(G.number_of_edges(), 4, "Expected four edges")
+			indexs = []
+			for key in G.nodes():
+				node = G.nodes()[key]
+				self.assertEqual(node['diff'], 0)
+				indexs.append(node['index'])
+			self.assertEqual(len(indexs), 4)
+			indexs.sort()
+			self.assertEqual(indexs, list(range(4)))
+
+	def test_figure_eight(self):
+		with tempfile.TemporaryDirectory("-allfed-spatial-test") as tempdir:
+			filename = os.path.join(tempdir, "testfile.shp")
+			features = [
+				Feature(LineString([(0, 0), (1, -1)]), {"some": "data"}),
+				Feature(LineString([(1, -1), (2, 0)]), {"some": "data"}),
+				Feature(LineString([(2, 0), (1, 1)]), {"some": "data"}),
+				Feature(LineString([(1, 1), (0, 0)]), {"some": "data"}),
+				Feature(LineString([(0, 0), (-1, -1)]), {"some": "data"}),
+				Feature(LineString([(-1, -1), (-2, 0)]), {"some": "data"}),
+				Feature(LineString([(-2, 0), (-1, 1)]), {"some": "data"}),
+				Feature(LineString([(-1, 1), (0, 0)]), {"some": "data"}),
+			]
+
+			featureIO.write_features(features, filename, "ESRI Shapefile")
+
+			G = nx.read_shp(tempdir)
+			preprocess_graph(G)
+
+			self.assertEqual(G.number_of_nodes(), 7, "Expected seven nodes")
+			self.assertEqual(G.number_of_edges(), 8, "Expected four edges")
+			indexs = []
+			for key in G.nodes():
+				node = G.nodes()[key]
+				self.assertEqual(node['diff'], 0)
+				indexs.append(node['index'])
+			self.assertEqual(len(indexs), 7)
+			indexs.sort()
+			self.assertEqual(indexs, list(range(7)))
+
+class Test_setup_min_cost_flow(LineBaseTest):
 	pass
